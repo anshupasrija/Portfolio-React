a44740314856f1725039e98dee0f792cf09e70d5
anshupasrija <anshu.pasrija@gmail.com>
2021-04-26 16:04:54 -0700

    Portfolio initial draft

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React,{Component}from 'react';
+import About from'./components/About';
+
+
+
+
+class App extends Component {
+  
+
+  render(){  
+    
+    return (
+      <About/>
+    );
+     
+         
+     
+
+}
+}
+
+export default App;
