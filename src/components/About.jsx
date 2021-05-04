10c5a2610a021e840210506ff317d423cffc0edc
anshupasrija <anshu.pasrija@gmail.com>
2021-05-04 10:40:13 -0700

    fourth push

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,15 @@ import '../styles/_about.scss'
 function About() {
     return (
         <div className="about">
-            
+           
+            <div className="about__subcontainer">
             <img className="about__picture" src={headicon} alt="profilepicture" />
             <div className="about__container">
-            <h1 className="about__heading">About</h1>
+            <p className="about__heading">Anshu Pasrija</p>
             <p className="about__paragraph">Hi, I am Anshu and I am a web developer based out of Vancouver,Canada.What drew me to web development was my belief that the power of technology will allow me to build systems that can help solve many of the world's current problems.I am eager to begin my career in this field at an organization where I can grow as a professional and individual. </p>
             </div>
+            </div>
+            {/* <h1 className="about__heading">About</h1> */}
         </div>
     )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,7 +22,7 @@ function Work() {
                     <img src={rentpic} alt="rentpic" className="work__image" />
                 <div className="work__info">
                     <p className="work__item__heading work__item__heading--right">Whats for Dinner</p>
-                    <p className="work__item__tech work__item__tech--right">REACT | SASS | JAVASCRIPT | EXPRESS | NODE.JS</p>
+                    <p className="work__item__tech work__item__tech--right">REACT | SASS | JAVASCRIPT </p>
                     <p>Whats for Dinner was developed for a 24 hour pair programming competition in which we came in SECOND PLACE. My partner and I decided we wanted to push our skills and create an interactive app for food, so we built whats for dinner. by using two API , one for meal and second for drink, it give you randon selction of complete meal.
                                 To build this application, we used React and JavaScript, as well as CSS/SASS for the front-end. For our server, we used Express along with Node.js..</p>
                 </div>
@@ -31,7 +31,7 @@ function Work() {
                 <img src={weather} alt="weather" className="work__image" />
                 <div className="work__info">
                     <p className="work__item__heading work__item__heading--right">Weather App</p>
-                    <p className="work__item__tech work__item__tech--right">REACT | SASS | JAVASCRIPT | EXPRESS | NODE.JS</p>
+                    <p className="work__item__tech work__item__tech--right">REACT | SASS | JAVASCRIPT </p>
                     <p>weather app i created to check the weather of any city by using openweather API.In this a user can enter any city and the app will show the country name ,weather with maximum and lowest temp with icon.
                      To build this application, I used React and JavaScript, as well as CSS/SASS for the front-end. For our server, we used Express along with Node.js..</p>
                 </div>
