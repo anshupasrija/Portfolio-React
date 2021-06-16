556a599dde686b75b4d2eaa970fa7b8764859467
anshupasrija <anshu.pasrija@gmail.com>
2021-06-15 17:22:11 -0700

    latest

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,7 @@ function topbar() {
         <div className="navbar">
             <div className="navbar__container">
                  <img src={logo} className="navbar__logo" alt="logo" /></div>
-                <ul className="navbar__subcontainer">
-                    {/* <li className="navbar__list">Home</li> */}
+                <ul className="navbar__subcontainer">                   
                     <li className="navbar__list">< a href="#about" className="navbar__link">About</a></li>
                     <li className="navbar__list"><a href="#work"className="navbar__link">Work</a></li>
                     <li className="navbar__list"><a href="#contact"className="navbar__link">Contact</a></li>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -14,7 +14,7 @@ function Work() {
                     <div className="work__info">
                     <p className="work__item__heading">Rent A Bin</p>
                     <p className="work__item__tech">REACT | SASS | JAVASCRIPT | EXPRESS | NODE.JS</p>
-                    <p className="work__item__paragraph">I developed Rent A Bin app while in school as my final capstone assignment. Given 10 days, we had to come up with an idea for a web application that would solve some type of issue. I built Rent A Bin, a application that allows you to book the plastic bins for moving .
+                    <p className="work__item__paragraph">I developed Rent A Bin app while in school as my final capstone assignment. Given 10 days, we had to come up with an idea for a web application that would solve some type of issue. I built Rent A Bin, an application that allows you to book the plastic bins for moving .
                                                         To build this application, I used React and JavaScript, as well as CSS/SASS for my front-end.For the back-end, I used Express and Node.js.</p>
                </div>
             </div>
@@ -24,7 +24,7 @@ function Work() {
                 <div className="work__info">
                     <p className="work__item__heading work__item__heading--right">Food Recipe Plaza</p>
                     <p className="work__item__tech work__item__tech--right">REACT | SASS | JAVASCRIPT </p>
-                    <p>Food Recipe Plaza was developed for a 24 hour pair programming competition in which we came in SECOND PLACE. My partner and I decided we wanted to push our skills and create an interactive app for food, so we built food Recipe Plaza. It give you randon selction of food according to ingridents.
+                    <p>Food Recipe Plaza was developed for a 24 hour pair programming competition in which we came in SECOND PLACE. My partner and I decided we wanted to push our skills and create an interactive app for food, so we built food Recipe Plaza. It give you random selction of food according to ingridents.
                                 To build this application, we used React and JavaScript, as well as CSS/SASS for the front-end. </p>
                 </div>
             </div>
